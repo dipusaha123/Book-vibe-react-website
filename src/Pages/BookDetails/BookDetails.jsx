7b3bb0fcc586ae72a3df876d7f70413b5aaa9391
shoumik saha <dipushoumiksaha@gmail.com>
2025-07-27 19:47:53 +0600

    final commit

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../Utility/addtoDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
     const { id } = useParams();
@@ -8,11 +12,17 @@ const BookDetails = () => {
     const data = useLoaderData();
     const singleBook = data.find(book => book.bookId === bookid)
     console.log(data)
-    const { bookName, image,author,review } = singleBook;
+    const { bookName, image, author, review } = singleBook;
 
-    const handleMarkAsRead = (id) =>{
+    const handleMarkAsRead = (id) => {
+        MySwal.fire({
+            title: "Good Job!",
+            icon: "success",
+            draggable: true
 
-       addToStoredDB(id)
+        })
+
+        addToStoredDB(id)
 
 
     }
@@ -21,21 +31,21 @@ const BookDetails = () => {
         <div className="card card-side  shadow-sm mt-7 ">
             <div className=' p-14 bg-[#131313]/5 rounded-4xl'  >
                 <img
-                className='w-[400px] h-[520px]  '
+                    className='w-[400px] h-[520px]  '
                     src={image}
                     alt="Movie" />
             </div>
             <div className="card-body">
                 <h2 className="card-title text-4xl ">{bookName}</h2>
                 <div>
-                    <p  className="text-gray-800 leading-none mt-4">By: {author}</p>
+                    <p className="text-gray-800 leading-none mt-4">By: {author}</p>
                     <div className="h-[1px] w-full bg-dotted border-b border-dotted border-gray-400 mt-5"></div>
                     <p className='mt-5'>Fiction</p>
                     <div className="h-[1px] w-full bg-dotted border-b border-dotted border-gray-400 mt-5 mb-3"></div>
                     <p>Review :this is a review pages</p>
                 </div>
                 <div className="card-actions justify-end">
-                    <button onClick={()=>handleMarkAsRead(id)} className="btn btn-primary">Mark As Read</button>
+                    <button onClick={() => handleMarkAsRead(id)} className="btn btn-primary">Mark As Read</button>
                     <button className="btn btn-primary">Add To Whishlist</button>
                 </div>
             </div>
